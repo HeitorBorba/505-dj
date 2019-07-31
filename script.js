4352e8045ce603e4687806af17b733c31d769c05
camilly <cacavh556@gmail.com>
2019-07-31 17:46:14 -0300

    Editar erros

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 
 let div = document.querySelector("div");
 let section = document.querySelector("section");
-let caixaTexto = document.querySelector("input");
+let texto = document.querySelector("input");
 let botao = document.querySelector("button");
-let texto = document.querySelector("h2");
-let naoSou = document.querySelector("a");
+let texto2 = document.querySelector("h2");
+let naoSouEu = document.querySelector("a");
 
 if(localStorage.nome){
     div.style.display = "none";
-    texto.innerHTML=`Seja bem vindo ${localStorage.nome}`;
-    naoSou.innerHTML=`Não sou ${localStorage.nome}`;
+    texto2.innerHTML=`Seja bem-vindo ${localStorage.nome}`;
+    naoSouEu.innerHTML=`Não sou ${localStorage.nome} ?`;
 }
 else{
     div.style.display="block";
     section.style.display="none";
     function cadastrar(){
-        localStorage.setItem("nome", caixaTexto.value);
+        localStorage.setItem("nome", texto.value);
         if(localStorage.nome){
             div.style.display = "none";
             section.style.display="block"
-            texto.innerHTML=`Seja bem vindo ${localStorage.nome}`;
-            naoSou.innerHTML=`Não sou ${localStorage.nome}`;
+            texto2.innerHTML=`Seja bem-vindo ${localStorage.nome}`;
+            naoSoueu.innerHTML=`Não sou ${localStorage.nome}?`;
         }
     }
 }
@@ -30,5 +30,5 @@ else{
     localStorage.clear();
 }
 
-botao.onclick = cadastrar;
+botao.onclick = Cadastre-se ;
 naoSou.onclick = limparStorage;
